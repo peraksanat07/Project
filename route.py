d06775cdcbe0b60098e392b9c624ad4925d49082
Peraksana Thayakaran <125240289+peraksanat07@users.noreply.github.com>
2024-06-26 14:13:06 +1200

    search bar works
    search bar works

--- route.py
+++ route.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from config import Config
 import sqlite3
 # import random
@@ -17,18 +17,46 @@ def homepage():
     return render_template("home.html")
 
 
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        # in other words, someone clicked submit, 'POSTing' info
+        # back to the server
+        search = request.form.get('searchterm')
+        con = sqlite3.connect(app.config['DATABASE'])
+        cur = con.cursor()
+        cur.execute("SELECT name, blurb FROM Books WHERE name LIKE ?", ('%' + search + '%',))
+        search1 = cur.fetchall()
+        cur = con.cursor()        
+        cur.execute("SELECT name, description FROM Genre WHERE name LIKE ?", ('%' + search + '%',))
+        search2 = cur.fetchall()
+        cur = con.cursor()        
+        cur.execute("SELECT name, description FROM Author WHERE name LIKE ?", ('%' + search + '%',))   
+        search3 = cur.fetchall()     
+        con.close()
+        # here is where you would do a query on your table for the search term
+
+        return render_template('search.html', search_term=search, books=search1, genres=search2, authors=search3)
+        # return f'You asked to search for \"{search, search1, search2, search3}\"'
+    else: 
+        return "nah"
+        
+
+
 @app.route('/about')
 def about():
     return render_template("layout.html")
 
+
 @app.route('/contact_us')
 def contact_us():
+
     return render_template("contact_us.html")
+
 
 @app.route('/log-in')
 def log_in():
     return render_template("login.html")
-
 
 @app.route('/author')
 def author():
@@ -47,10 +75,12 @@ def author_details(id):
     cur.execute("SELECT * FROM Author WHERE id=?;",(id,))
     author = cur.fetchone()
     cur = con.cursor()
-    cur.execute("SELECT * FROM book_author WHERE book=author;",(author,))
-    book_author = cur.fetchone()
+    # cur.execute("SELECT book FROM book_author WHERE author=?;",(author,))
+    # book_id = cur.fetchall()
+    cur.execute("SELECT id, name FROM Books WHERE id IN (SELECT book FROM book_author WHERE author=?)",(id,))
+    books = cur.fetchall()
     con.close()
-    return render_template("a_details.html", author=author, book_author=book_author)
+    return render_template("a_details.html", author=author, books=books)
 
 
 @app.route('/books')
@@ -69,8 +99,13 @@ def book_details(id):
     cur = con.cursor()
     cur.execute("SELECT * FROM Books WHERE id=?;",(id,))
     book = cur.fetchone()
+    cur.execute("SELECT id, name FROM Genre WHERE id IN (SELECT genre FROM book_genre WHERE book=?)",(id,))
+    genre = cur.fetchall()
+    cur = con.cursor()
+    cur.execute("SELECT id, name FROM Author WHERE id IN (SELECT author FROM book_author WHERE book=?)",(id,))
+    authors = cur.fetchall()
     con.close()
-    return render_template("b_details.html", book=book)
+    return render_template("b_details.html", book=book, genre=genre, authors=authors)
 
 
 @app.route('/genre')
